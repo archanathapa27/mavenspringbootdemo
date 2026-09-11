a3ae68feae1ee14a7c52754a13401a3f6112e398
Archana Thapa <archana27.thapa@gmail.com>
2026-09-11 16:25:08 +0530

    added actuator and monitoring

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -10,4 +10,10 @@ public class HelloController {
     public String index() {
         return "Greetings from Spring Boot!";
     }
+
+    @GetMapping("/slow")
+    public String slowEndpoint() throws InterruptedException {
+        Thread.sleep(5000);
+        return "Done, that took a while";
+    }
 }
